c58ec9f7176114630e08b5aca74ab2be94b7053e
siva-byte <sivaparvathaneni69@gmail.com>
2020-03-21 21:00:47 +0530

    navigate

---
+++ seliniumconfigure/src/navigatesel/Navigation1.java
@@ -0,0 +1,28 @@
+package navigatesel;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Navigation1 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\DELL\\Downloads\\chromedriver_win32 siva/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+        driver.get("https://demo.nopcommerce.com/login?returnUrl=%2F");
+        driver.manage().window().maximize();
+        System.out.println(driver.getTitle());
+        
+        driver.navigate().to("https://www.facebook.com/");
+        System.out.println(driver.getTitle());
+        driver.navigate().back();
+        System.out.println(driver.getTitle());
+        driver.navigate().forward();System.out.println(driver.getTitle());
+        
+        driver.navigate().refresh();
+        driver.close();
+        
+        
+
+	}
+
+}
